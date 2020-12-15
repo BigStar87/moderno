7d2a532f393f73d50df9999ed5e91858c3a4ce2d
BigStar87 <Alexlialin@mail.ru>
2020-12-15 19:54:19 +0400

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,17 @@ $(function(){
 
 var mixer = mixitup('.products__inner-box')
 
+$('.slider__carousel').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false
+});
+
+$(".rate-star").rateYo({
+  rating: 4,
+  starWidth: "12px",
+  readOnly: true
+});
+
 });
